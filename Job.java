0450da4e18e6242d743072d6886980109a967f1b
Dillon O'Connor <37981696+DillonOC@users.noreply.github.com>
2024-10-21 22:09:17 +0100

    added doc comments to Job class

--- Job.java
+++ Job.java
@@ -1,18 +1,30 @@
+/**
+ * The Job class implements the Runnable interface.
+ * Each job has a job number and is processed by a thread.
+ */
 public class Job implements Runnable {
     private int jobNumber;
 
+    /**
+     * Constructor to create a Job with a specific job number.
+     *
+     * @param jobNumber The number assigned to this job.
+     */
     Job(int jobNumber) {
         this.jobNumber = jobNumber;
     }
 
+    /**
+     * The run method processes the job by simulating work (sleeping).
+     * It prints the current thread name before and after the work is done.
+     */
     public void run() {
-        // Undertake required work, here we will emulate it by sleeping for a period
         System.out.println("Job: " + jobNumber + " is being processed by thread: " 
                             + Thread.currentThread().getName());
         try {
             Thread.sleep(1000);
         } catch (InterruptedException e) {
-            // no catching as example should not experience interruptions
+            // No handling required for this example
         }
         System.out.println("Job: " + jobNumber + " is ending in thread: " 
                             + Thread.currentThread().getName());
